9ce60cc0e9baace840cd9b5a3737731c785e4537
Jahus <jahus@live.ie>
2023-08-01 11:47:43 +0100

    Allow use of multiple accounts and multiple websites/campaigns

--- PopCashCampaignUpdaterRSS.py
+++ PopCashCampaignUpdaterRSS.py
@@ -20,7 +20,7 @@ class DoubleOut:
         self.file.flush()
         
 
-__last_processed_timestamp = None
+__last_processed_timestamp = {}
 __log_file = open("latest.log", 'a', encoding="utf-8")
 sys.stdout = DoubleOut(__log_file)
 
@@ -34,21 +34,22 @@ def pc_increase_campaign_budget(campaign_id, api_key, budget_delta):
     data = {
         "adjustBudget": 0.1 + ((budget_delta - 0.1) * int(not __config["debug"]))
     }
-    response = requests.put(url, headers=headers, json=data)
+    response = requests.put(url, headers=headers, json=data, timeout=10)
     return (response.status_code == 200)
 
 
-def check_new_article_in_rss(feed_url, authors):
-    global __last_processed_timestamp
-    feed = feedparser.parse(feed_url)
-    if 'entries' in feed:
-        for _latest_article in feed.entries[0:3]:
-            _article_timestamp = _latest_article.get('published_parsed')
-            _article_id = _latest_article.get('id')
-            if (_latest_article.get('author') in authors) and (__last_processed_timestamp is None or _article_timestamp > __last_processed_timestamp):
-                __last_processed_timestamp = _article_timestamp
-                return True, _article_id
-        return False, feed.entries[0].get('id')
+def check_new_article_in_rss(website):
+    feed = feedparser.parse(website["rss_link"])
+    if "entries" in feed:
+        if len(feed["entries"]) > 0:
+            for _latest_article in feed.entries[0:3]:
+                _article_timestamp = _latest_article.get('published_parsed')
+                _article_id = _latest_article.get('id')
+                if (_latest_article.get('author') in website["authors"] or len(website["authors"]) == 0) and (website["rss_link"] not in __last_processed_timestamp or _article_timestamp > __last_processed_timestamp[website["rss_link"]]):
+                    __last_processed_timestamp[website["rss_link"]] = _article_timestamp
+                    return True, _article_id
+            return False, feed.entries[0].get('id')
+        return False, None
     return False, None
 
 
@@ -58,16 +59,17 @@ if __name__ == "__main__":
         __config = json.loads(_config_file.read())
     
     while True:
-        print("%s\n Getting updates via RSS..." % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        new_article_available, article_id = check_new_article_in_rss(__config["rss_link"], __config["authors"])
-        if new_article_available:
-            if pc_increase_campaign_budget(__config["campaign_id"], __config["api_key"], __config["budget_delta"]):
-                print(f"Campaign {__config['campaign_id']} budget increased successfully by {__config['budget_delta']} $\n for new article {article_id}\n published on %s." % time.strftime("%Y-%m-%d %H:%M:%S", __last_processed_timestamp))
+        for _website in __config["websites"]:
+            print("%s\n Getting updates via RSS for %s..." % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), _website))
+            new_article_available, article_id = check_new_article_in_rss(__config["websites"][_website])
+            if new_article_available:
+                if pc_increase_campaign_budget(__config["websites"][_website]["campaign_id"], __config["websites"][_website]["api_key"], __config["websites"][_website]["budget_delta"]):
+                    print(f"Campaign {__config['websites'][_website]['campaign_id']} budget increased successfully by {__config['websites'][_website]['budget_delta']} $\n for new article {article_id}\n published on %s." % time.strftime("%Y-%m-%d %H:%M:%S", __last_processed_timestamp[__config["websites"][_website]["rss_link"]]))
+                else:
+                    print(f"Failed to increase budget for campaign {__config['websites'][_website]['campaign_id']}.")
             else:
-                print(f"Failed to increase budget for campaign {__config['campaign_id']}.")
-        else:
-            print(f"No new articles available in the RSS feed.\n Last article: {article_id}.")
-        print("Waiting %i seconds before checking for updates...\nPress CTRL+C to exit." % (__config["update_period"] * (1 + 59 * int(not __config["debug"]))), end='\r')
+                print(f"No new articles available in the RSS feed.\n Last article: {article_id}.")
+            print("Waiting %i seconds before checking for updates...\n" % (__config["update_period"] * (1 + 59 * int(not __config["debug"]))))
         time.sleep(__config["update_period"] * (1 + 59 * int(not __config["debug"])))
 
 sys.stdout = sys.__stdout__
