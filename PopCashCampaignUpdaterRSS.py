615ae5da46a9d09d4a95e7c619860bc9af6be5e3
Jahus <jahus@live.ie>
2024-03-23 21:27:11 +0100

    Add update_url and append_utm options

--- PopCashCampaignUpdaterRSS.py
+++ PopCashCampaignUpdaterRSS.py
@@ -25,7 +25,7 @@ __log_file = open("latest.log", 'a', encoding="utf-8")
 sys.stdout = DoubleOut(__log_file)
 
 
-def pc_increase_campaign_budget(campaign_id, api_key, budget_delta):
+def pc_increase_campaign_budget(campaign_id, api_key, budget_delta, update_url=False):
     url = f"https://api.popcash.net/campaigns/{campaign_id}"
     headers = {
         "X-Api-Key": api_key,
@@ -34,6 +34,8 @@ def pc_increase_campaign_budget(campaign_id, api_key, budget_delta):
     data = {
         "adjustBudget": 0.1 + ((budget_delta - 0.1) * int(not __config["debug"]))
     }
+    if update_url:
+        data["url"] = update_url
     response = requests.put(url, headers=headers, json=data, timeout=10)
     return (response.status_code == 200)
 
@@ -45,12 +47,13 @@ def check_new_article_in_rss(website):
             for _latest_article in feed.entries[0:3]:
                 _article_timestamp = _latest_article.get('published_parsed')
                 _article_id = _latest_article.get('id')
+                _article_url = _latest_article.get('link')
                 if (_latest_article.get('author') in website["authors"] or len(website["authors"]) == 0) and (website["rss_link"] not in __last_processed_timestamp or _article_timestamp > __last_processed_timestamp[website["rss_link"]]):
                     __last_processed_timestamp[website["rss_link"]] = _article_timestamp
-                    return True, _article_id
-            return False, feed.entries[0].get('id')
-        return False, None
-    return False, None
+                    return True, _article_id, _article_url
+            return False, feed.entries[0].get('id'), feed.entries[0].get('link')
+        return False, None, None
+    return False, None, None
 
 
 if __name__ == "__main__":
@@ -60,16 +63,24 @@ if __name__ == "__main__":
     
     while True:
         for _website in __config["websites"]:
-            print("%s\n Getting updates via RSS for %s..." % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), _website))
-            new_article_available, article_id = check_new_article_in_rss(__config["websites"][_website])
-            if new_article_available:
-                if pc_increase_campaign_budget(__config["websites"][_website]["campaign_id"], __config["websites"][_website]["api_key"], __config["websites"][_website]["budget_delta"]):
-                    print(f"Campaign {__config['websites'][_website]['campaign_id']} budget increased successfully by {__config['websites'][_website]['budget_delta']} $\n for new article {article_id}\n published on %s." % time.strftime("%Y-%m-%d %H:%M:%S", __last_processed_timestamp[__config["websites"][_website]["rss_link"]]))
+            if __config["websites"][_website]["rss_link"]:
+                print("%s\n Getting updates via RSS for %s..." % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), _website))
+                new_article_available, article_id, article_url = check_new_article_in_rss(__config["websites"][_website])
+                if not __config["websites"][_website]["update_url"]:
+                    article_url = False
+                if __config["websites"][_website]["append_utm"]:
+                    if '?' in article_url:
+                        article_url += "&" + __config["websites"][_website]["append_utm"]
+                    else:
+                        article_url += "?" + __config["websites"][_website]["append_utm"]
+                if new_article_available:
+                    if pc_increase_campaign_budget(__config["websites"][_website]["campaign_id"], __config["websites"][_website]["api_key"], __config["websites"][_website]["budget_delta"], update_url=article_url):
+                        print(f"Campaign {__config['websites'][_website]['campaign_id']} budget increased successfully by {__config['websites'][_website]['budget_delta']} $\n for new article {article_id}\n published on %s." % time.strftime("%Y-%m-%d %H:%M:%S", __last_processed_timestamp[__config["websites"][_website]["rss_link"]]))
+                    else:
+                        print(f"Failed to increase budget for campaign {__config['websites'][_website]['campaign_id']}.")
                 else:
-                    print(f"Failed to increase budget for campaign {__config['websites'][_website]['campaign_id']}.")
-            else:
-                print(f"No new articles available in the RSS feed.\n Last article: {article_id}.")
-            print("Waiting %i seconds before checking for updates...\n" % (__config["update_period"] * (1 + 59 * int(not __config["debug"]))))
+                    print(f"No new articles available in the RSS feed.\n Last article: {article_id}.")
+                print("Waiting %i seconds before checking for updates...\n" % (__config["update_period"] * (1 + 59 * int(not __config["debug"]))))
         time.sleep(__config["update_period"] * (1 + 59 * int(not __config["debug"])))
 
 sys.stdout = sys.__stdout__
--- readme.md
+++ readme.md
@@ -5,19 +5,23 @@ Create a `config.json` file and follow this template:
 ```json
 {
 	"websites": {
-		"My Super News Website": {
+		"my.super-news.web": {
 			"api_key": "such-sicret-key",
 			"campaign_id": "123490",
 			"rss_link": "https://awesome-doma.in/feed/",
 			"budget_delta": 1,
-			"authors": ["NotChatGPT", "Spectalist"]
+			"authors": ["NotChatGPT", "Spectalist"],
+            "update_url": false,
+            "append_utm": "utm_source=popcash"
 		},
-		"My Not Copy Pasta Blog": {
+		"not-copy-pasta.blog": {
 			"api_key": "wow-other-key",
 			"campaign_id": "119181",
-			"rss_link": "https://googl-disco.net/feed/",
+			"rss_link": false,
 			"budget_delta": 2,
-			"authors": []
+			"authors": [],
+            "update_url": true,
+            "append_utm": false
 		}
 	},
 	"update_period": 5,
@@ -25,3 +29,15 @@ Create a `config.json` file and follow this template:
 }
 
 ```
+
+## Notes
+
+- Use your domain name as website name in the config file.
+
+- If `authors` is empty (`[]`), the script will not filter authors.
+
+- If `update_url` is `true`, the script will push article URL to the campaign. However, be aware that PopCash will put the campaign on hold ("Pending") until they review and validate the URL.
+
+- If you want to set UTM parameters, do so in `append_utm`. If not, set it to `false`.
+
+- `PopCashClipboard.py` will read from clipboard and, if it finds a valid URL, will update the campaign corresponding to the URL's domain name.
